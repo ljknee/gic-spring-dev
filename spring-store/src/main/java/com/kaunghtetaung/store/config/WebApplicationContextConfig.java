e91eecb098cb4348d8c133054cc95370f388ed3e
kaunghtetaung <kaunghtetaung@DESKTOP-JQJIG8A>
2018-07-02 14:27:37 +0900

    Model, View, Controller Relation & Request Flow

---
+++ spring-store/src/main/java/com/kaunghtetaung/store/config/WebApplicationContextConfig.java
@@ -0,0 +1,34 @@
+package com.kaunghtetaung.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.kaunghtetaung.store")
+public class WebApplicationContextConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+	
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		
+		return resolver;
+	}
+
+}
